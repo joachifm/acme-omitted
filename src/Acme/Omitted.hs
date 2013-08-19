a4ccc444ec7ae2edcc0b48c87c34e2b20a601ae7
Joachim Fasting <joachim.fasting@gmail.com>
2013-08-19 23:56:14 +0200

    Differentiate the merely omitted from the truly undefined
    Note that these definitions require the use of Control.Exception,
    which means no more Haskell98 compatibility.

--- src/Acme/Omitted.hs
+++ src/Acme/Omitted.hs
@@ -13,7 +13,9 @@ This makes it possible to differentiate between the truly \"undefined\"
 and the merely \"omitted\".
 -}
 
-module Acme.Omitted ((...), omitted) where
+module Acme.Omitted ((...), omitted, isOmitted, isUndefined) where
+
+import qualified Control.Exception as E
 
 -- | Alternative syntax for 'omitted' that has been carefully
 -- optimised for programmer convenience and visual presentation
@@ -30,3 +32,27 @@ module Acme.Omitted ((...), omitted) where
 -- This is sufficient to express _all_ types of omitted content
 omitted :: a
 omitted = error "omitted"
+
+-- | Answer the age-old question \"was this definition omitted?\"
+--
+-- @
+-- isOmitted 0         = return False
+-- isOmitted undefined = return False
+-- isOmitted omitted   = return True
+-- @
+isOmitted :: a -> IO Bool
+isOmitted = isErrorCall "omitted"
+
+-- | ... or is it really 'undefined'?
+--
+-- @
+-- isUndefined 0         = return False
+-- isUndefined omitted   = return False
+-- isUndefined undefined = return True
+-- @
+isUndefined :: a -> IO Bool
+isUndefined = isErrorCall "Prelude.undefined"
+
+isErrorCall :: String -> a -> IO Bool
+isErrorCall s x = (E.evaluate x >> return False)
+                  `E.catch` (\(E.ErrorCall e) -> return $ e == s)
