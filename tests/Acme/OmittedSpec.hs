ea6fed432ae4c7427af9a598561b08ad12aa92c6
Joachim Fasting <joachim.fasting@gmail.com>
2013-08-20 10:46:46 +0200

    Test the behaviour of isOmitted and isUndefined

--- tests/Acme/OmittedSpec.hs
+++ tests/Acme/OmittedSpec.hs
@@ -10,3 +10,17 @@ spec = do
   describe "omitted" $ do
     it "denotes omitted content" $ do
       omitted `shouldThrow` errorCall "omitted"
+  describe "isOmitted" $ do
+    it "isOmitted 0         = False" $ do
+      isOmitted 0 `shouldReturn` False
+    it "isOmitted undefined = False" $ do
+      isOmitted undefined `shouldReturn` False
+    it "isOmitted omitted   = True" $ do
+      isOmitted omitted  `shouldReturn` True
+  describe "isUndefined" $ do
+    it "isUndefined 0         = False" $ do
+      isUndefined 0 `shouldReturn` False
+    it "isUndefined undefined = True" $ do
+      isUndefined undefined `shouldReturn` True
+    it "isUndefined omitted   = False" $ do
+      isUndefined omitted  `shouldReturn` False
