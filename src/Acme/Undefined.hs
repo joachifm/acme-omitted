6b0db501c74c6e3ac1495293bf3731ca9ddcbc4b
Joachim Fasting <joachifm@fastmail.fm>
2016-07-24 22:57:25 +0200

    Acme.Undefined: better haddocks

--- src/Acme/Undefined.hs
+++ src/Acme/Undefined.hs
@@ -7,36 +7,17 @@ License     : BSD3
 Maintainer  : joachifm@fastmail.fm
 Stability   : stable
 Portability : portable
-
-This module provides an alternative implementation of
-\"Prelude.undefined\", intended exclusively for denoting
-truly undefinable values.
 -}
 
 module Acme.Undefined (
-    -- * \"Undefined\" redefined
-    --
-    -- $undefined
     undefined
   ) where
 
 import Prelude hiding (undefined)
 
-{-$undefined
-
-Lacking a dedicated name for omitted defintions, users of Standard
-Haskell are left with no choice but to use \"undefined\" for both
-the undefinable and the omitted.
-This makes the standard implementation of \"undefined\" deficient, we
-cannot be sure what the programmer has intended, only that the definition is
-missing.
-The following implementation of undefined is similar in every way to
-the standard implementation, but is free from conceptual contamination.
+{-|
+Denotes all values that are, fundamentally, undefinable.  The contract of
+'undefined' is that it will never be used for merely omitted definitions.
 -}
-
--- | Denotes all values that are, fundamentally, undefinable.
---
--- The contract of 'undefined' is that it will never be used for
--- merely omitted definitions.
 undefined :: a
 undefined = error "Acme.Undefined.undefined"
